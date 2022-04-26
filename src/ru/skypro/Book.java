1ef6ea405dc1dab8d70974e3dc578bf965359d63
Ян <Shatoshi@mail.ru>
2022-04-26 20:55:14 +0300

    Выполненное домашнее задание (вторая часть)

--- src/ru/skypro/Book.java
+++ src/ru/skypro/Book.java
@@ -1,5 +1,7 @@
 package ru.skypro;
 
+import java.util.Objects;
+
 public class Book {
     String name;
     int publicationYear;
@@ -10,6 +12,7 @@ public class Book {
         this.publicationYear = publicationYear;
         this.author = author;
     }
+
     public String getBookName() {
         return this.name;
     }
@@ -22,5 +25,30 @@ public class Book {
         this.publicationYear = publicationYear;
     }
 
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    @Override
+    public String toString() {
+        return "'" + name + "' " + "published in " + publicationYear + " by " + this.author.toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (this.getClass() != other.getClass() || other == null) {
+            return false;
+        }
+        Book b2 = (Book) other;
+        return (name.equals(b2.name) && author.equals(b2.author));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, publicationYear, author);
+    }
 }
 
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -9,15 +9,23 @@ public class Main {
         Book secondBook = new Book("Principles of Plasma Discharges and Materials Processing", 2005, secondAuthor);
 
         delimiter();
-        System.out.println("'" + firstBook.getBookName() + "'" + " published in " + firstBook.getPublicationYear() + " by " + firstAuthor.getAuthorName() + " " + firstAuthor.getAuthorSecondName());
+        System.out.println("'" + firstBook.getBookName() + "'" + " published in " + firstBook.getPublicationYear() + " by " + firstBook.getAuthor().getAuthorName() + " " + firstBook.getAuthor().getAuthorSecondName());
         delimiter();
 
         firstBook.setPublicationYear(2003);
 
-        System.out.println("'" + firstBook.getBookName() + "'" + " published in " + firstBook.getPublicationYear() + " by " + firstAuthor.getAuthorName() + " " + firstAuthor.getAuthorSecondName());
+        System.out.println("'" + firstBook.getBookName() + "'" + " published in " + firstBook.getPublicationYear() + " by " + firstBook.getAuthor().getAuthorName() + " " + firstBook.getAuthor().getAuthorSecondName());
         delimiter();
-        System.out.println("'" + secondBook.getBookName() + "'" + " published in " + secondBook.getPublicationYear() + " by " + secondAuthor.getAuthorName() + " " + secondAuthor.getAuthorSecondName());
+        System.out.println("'" + secondBook.getBookName() + "'" + " published in " + secondBook.getPublicationYear() + " by " + secondBook.getAuthor().getAuthorName() + " " + secondBook.getAuthor().getAuthorSecondName());
         delimiter();
+
+        // Объекты и классы ч. 2
+
+        System.out.println(firstBook.toString());
+        Author thirdAuthor = new Author("Jonathan", "Safran Foer");
+        Book thirdBook = new Book("Everything Is Illuminated", 2004, thirdAuthor);
+        System.out.println("Author Jonathan Safran Foer is already in the database? - " + thirdAuthor.equals(firstAuthor));
+        System.out.println("'" + "Everything is Illuminated" + "' with the same author is already in the database? - " + thirdBook.equals(firstBook));
     }
 
     public static void delimiter() {
